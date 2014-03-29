3435e7be87d17dc6fec2d82baabc27183b42e9d1
Christopher Fagiani <christopher.fagiani@gmail.com>
2014-03-28 20:41:46 -0400

    switched to vectorLayer

--- api/public/js/map.js
+++ api/public/js/map.js
@@ -1,20 +1,22 @@
 var map = null;
 var markers = null;
 
+var epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+var projectTo = new OpenLayers.Projection("EPSG:900913"); //The map projection (Spherical Mercator)
+
+
+
 function init() {
     map = new OpenLayers.Map("mapdiv");
     var mapnik = new OpenLayers.Layer.OSM();
     map.addLayer(mapnik);
 
 
-    var lonlat = new OpenLayers.LonLat(-78.865002, 42.901206).transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator
-    );
+    var lonlat = new OpenLayers.LonLat(-78.865002, 42.901206).transform(epsg4326, projectTo);
 
     var zoom = 15;
 
-    markers = new OpenLayers.Layer.Markers("Markers");
+    markers =  new OpenLayers.Layer.Vector("Overlay");
     map.addLayer(markers);
 
     map.setCenter(lonlat, zoom);
@@ -31,16 +33,18 @@ function fetchPoints() {
     topRight.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
 
     $.get("/healthinspections/facility?blx=" + bottomLeft.x + "&bly=" + bottomLeft.y + "&trx=" + topRight.x + "&try=" + topRight.y, function (data) {
-        markers.clearMarkers();
+        markers.removeAllFeatures();
         $.each(data, function (index, value) {
             if (value.loc !== undefined) {
-                var lonlat = new OpenLayers.LonLat(value.loc.coordinates[0], value.loc.coordinates[1]).transform(
-                    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-                    new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator
-                );
-                var marker =new OpenLayers.Marker(lonlat);
 
-                markers.addMarker(marker);
+                var marker = new OpenLayers.Feature.Vector(
+                    new OpenLayers.Geometry.Point( value.loc.coordinates[0], value.loc.coordinates[1]).transform(epsg4326, projectTo),
+                    {description:'This is the value of<br>the description attribute'} ,
+                    {externalGraphic: '/images/marker-red.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                );
+
+                markers.addFeatures(marker);
+
                 /*marker.events.register('mousedown', marker, function(evt) {
 
                 });*/
