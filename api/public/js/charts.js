1330704ecdf0e247de6fbd5c8f597dcfed18e358
Christopher Fagiani <christopher.fagiani@gmail.com>
2014-03-29 21:49:58 -0400

    added drilldown to violation chart

--- api/public/js/charts.js
+++ api/public/js/charts.js
@@ -1,24 +1,24 @@
 function initCharts() {
     initViolationChart();
-    $('ul.nav.nav-pills li a').click(function(evt) {
+    $('ul.nav.nav-pills li a').click(function (evt) {
         clearChart();
-        if(evt.target.id == 'violations-link'){
+        if (evt.target.id == 'violations-link') {
             initViolationChart();
-        }else if (evt.target.id == 'inspections-link'){
+        } else if (evt.target.id == 'inspections-link') {
             initInspectionChart();
-        }else if (evt.target.id == 'facility-link'){
+        } else if (evt.target.id == 'facility-link') {
             initFacilityTypeChart();
         }
 
     });
 }
 
-function clearChart(){
+function clearChart() {
     $("#chart-heading").empty();
     $("#chart-content").empty();
 }
 
-function initFacilityTypeChart(){
+function initFacilityTypeChart() {
     $.get("/healthinspections/facility/stats?type=facilitytype", function (data) {
 
         var dataVals = [];
@@ -39,7 +39,7 @@ function initFacilityTypeChart(){
 
             series: [
                 {
-                    type:"pie",
+                    type: "pie",
                     data: dataVals
                 }
             ]
@@ -47,7 +47,7 @@ function initFacilityTypeChart(){
     });
 }
 
-function initInspectionChart(){
+function initInspectionChart() {
     $.get("/healthinspections/inspection/stats?type=inspections", function (data) {
 
         var dataVals = [];
@@ -68,7 +68,7 @@ function initInspectionChart(){
 
             series: [
                 {
-                    type:"pie",
+                    type: "pie",
                     data: dataVals
                 }
             ]
@@ -78,22 +78,34 @@ function initInspectionChart(){
 
 function initViolationChart() {
     $.get("/healthinspections/inspection/stats?type=violations", function (data) {
-        var threshold = 30;
+        var threshold = 200;
+        var secondThreshold = 30;
         var categories = [];
         var dataVals = [];
         var excludedCount = 0;
+        var secondExclusionCount = 0;
+        var drilldownData = [];
+        var secondDrillData = [];
         $.each(data, function (index, value) {
-            if(value.count > threshold) {
+            if (value.count > threshold) {
                 categories.push(value.text);
-                dataVals.push(value.count);
-            }else{
-                excludedCount+= value.count;
+                dataVals.push({name: value.text, y: value.count});
+            } else if (value.count > secondThreshold) {
+                excludedCount += value.count;
+                drilldownData.push([value.text, value.count]);
+            } else {
+                secondExclusionCount += value.count;
+                secondDrillData.push([value.text, value.count]);
             }
 
         });
-        if(excludedCount>0){
-            categories.push("other");
-            dataVals.push(excludedCount);
+        if (excludedCount > 0) {
+            categories.push("Other");
+            dataVals.push({name: "Other", y: excludedCount, drilldown: "Other"});
+            if (secondExclusionCount > 0) {
+                drilldownData.push({name: "Others", y: secondExclusionCount, drilldown: "Other2"})
+            }
+
         }
         $("#chart-heading").html("<h3>All-time violations</h3>")
         $('#chart-content').highcharts({
@@ -116,10 +128,22 @@ function initViolationChart() {
                     name: 'Violations',
                     data: dataVals
                 }
-            ]
+            ], legend: {
+                enabled: false
+            },
+            drilldown: {
+                series: [
+                    {
+                        id: "Other",
+                        data: drilldownData
+                    },
+                    {id: "Other2",
+                        data: secondDrillData
+                    }
+                ]
+            }
         });
     });
-
 
 
 }
